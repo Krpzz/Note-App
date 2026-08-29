bf0df9351fba0062ee412e9fda72ff1b5c6dc2ab
Krpzz <krpz54919@gmail.com>
2026-08-29 08:50:15 +0545

    layout

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import os
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pymongo import MongoClient
-from pymongo.errors import PyMongoError
+from pymongo import MongoClient # type: ignore
+
 
 app = FastAPI()
 
@@ -13,48 +11,23 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-
-mongo_client = MongoClient(
-    os.getenv("MONGODB_URI", "mongodb://localhost:27017/"),
-    serverSelectionTimeoutMS=2000,
-)
-mongo_database = os.getenv("MONGODB_DATABASE", "notes")
-mongo_collection = os.getenv("MONGODB_COLLECTION", "notes")
-
-
-def get_mongodb_data():
-    documents = list(mongo_client[mongo_database][mongo_collection].find({}).limit(20))
-
-    for document in documents:
-        document["_id"] = str(document["_id"])
-
-    return documents
-
+conn = MongoClient("mongodb+srv://kripeshkhadka66_db_user:sqf7J47nQ7wNnEg7@notes.43zz0yx.mongodb.net")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    mongodb_data = []
-    mongodb_error = None
-
-    try:
-        mongodb_data = get_mongodb_data()
-    except PyMongoError as exc:
-        mongodb_error = str(exc)
-
+async def read_item(request: Request,):
+    docs = conn.notes.notes.find({})
+    newDocs = []
+    for doc in docs:
+        newDocs.append({ # type: ignore
+            "id": doc["_id"],
+            "note": doc["note"]
+        })
     return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={
-            "mongodb_data": mongodb_data,
-            "mongodb_error": mongodb_error,
-            "mongo_database": mongo_database,
-            "mongo_collection": mongo_collection,
-        },
+        request=request, name="index.html", context={"newDocs": ["newDocs"]}
     )
-
 
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"id": id}
+        request=request, name="item.html", context={"id": id}
     )
